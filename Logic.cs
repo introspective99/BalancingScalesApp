581e0c5bbc0dfcfd1a6a5a9b5e014a14b626a9bb
introspective99 <49716304+introspective99@users.noreply.github.com>
2020-01-21 17:09:06 +0000

    Initial commit

---
+++ Logic.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BalancingScalesApp
+{
+    public static class Logic
+    {
+        public static int[] balancer(int[] scales, int[] weights)
+        {
+            for (int x = 0; x < weights.Length; x++)
+            {
+                for (int y = 0; y < weights.Length; y++)
+                {
+                    //block 1
+                    if (scales[0] + weights[x] == scales[1] + weights[y])
+                    {
+                        return new int[] { weights[x], weights[y] };
+                    }
+                    //block 2
+                    if (scales[0] + weights[y] == scales[1] + weights[x])
+                    {
+                        return new int[] { weights[y], weights[x] };
+                    }
+                    //block 3
+                    if (scales[0] + weights[x] + weights[y] == scales[1])
+                    {
+                        return new int[] { weights[y], weights[x] };
+                    }
+                    //block 4
+                    if (scales[1] + weights[x] + weights[y] == scales[0])
+                    {
+                        return new int[] { weights[y], weights[x] };
+                    }
+                    //block 5
+                    if (scales[0] + weights[x] == scales[1])
+                    {
+                        return new int[] { weights[x] };
+                    }
+                    //block 6
+                    if (scales[0] + weights[y] == scales[1])
+                    {
+                        return new int[] { weights[y] };
+                    }
+                    //block 7
+                    if (scales[1] + weights[x] == scales[1])
+                    {
+                        return new int[] { weights[x] };
+                    }
+                    //block 8
+                    if (scales[1] + weights[y] == scales[1])
+                    {
+                        return new int[] { weights[y] };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BalancingScalesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scales = { 6, 8 };
+            int[] weights = { 3, 4, 5, 8 };
+
+            int[] scales2 = { 1, 2 };
+            int[] weights2 = { 4, 7, 9, 1 };
+
+            int[] scales3 = { 5, 9 };
+            int[] weights3 = { 1, 3, 6, 7 };
+
+            int[] scales4 = { 3, 6 };
+            int[] weights4 = { 2, 6, 7, 19 };
+
+        }
+    }
+}
